b1317a067ac3ab094f6dc3fb16654c7d0412538b
"SandovaI" <"calebsandoval901@gmail.com">
2022-02-06 22:57:15 -0800

    third

--- purchase.sol
+++ purchase.sol
@@ -3,12 +3,16 @@ pragma solidity ^0.8.10;
 contract purchase{
     int public profits;
     string public storeName = 'Calebs Store';
+    int[] public orders;
+
     //Functions
+    // a
     function buy(int price) public{
+        orders.push(price);
         profits += price;
     }
-    function refund(int price) public{
-        profits -= price;
+    function refund(uint orderNumber) public{
+        profits -= orders[orderNumber];
     }
     function seeProfits(bool realProfits) public view returns (int) {
         if(realProfits){
